c6f64eaf4ed6f44dc45535d915a056ebae2611eb
Rajesh <245122733172@mvsrec.edu.in>
2023-05-05 23:08:16 -0400

    adding the files which have comments

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const ABC = abc.map(letter => letter.toUpperCase());
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const symbols = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
 
+// creating an object of arrays
 const arrayOfArrays = {
     "abc": abc,
     "ABC": ABC,
@@ -13,8 +14,10 @@ const arrayOfArrays = {
     "!#&": symbols
 }
 
+// initializing a random array with empty value
 let randomArray = [];
 
+// selecting all the elements from the DOM
 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 let slider = document.getElementById("slider");
 let output = document.getElementById("output");
@@ -26,12 +29,9 @@ let icon2 = document.getElementById("icon2");
 let download1 = document.getElementById("download1");
 let download2 = document.getElementById("download2");
 
+// creating a password generator function
 function passwordGenerator(array, value) {
     let password = "";
-    // let randomArray = 
-    // create a random array by concatenating the array names
-
-    // loop through the array and generate a password of length of the value
     for (let i = 0; i < value; i++) {
         // get a random number
         let randomNumber = Math.floor(Math.random() * array.length);
@@ -44,18 +44,27 @@ function passwordGenerator(array, value) {
     return password;
 }
 
+// creating a function to generate a random text file
 function generateRandomTextFile(name, data, reason) {
+    // get the current date and time
     let date = new Date();
     let dateTime = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes();
+    // creating a content for the text file
     const text = "password: " + data + "\n" + "reason: " + reason + "\n" + "date: " + dateTime;
+    // creating a file name
     const filename = name + ".txt";
+    // creating a blob
     const blob = new Blob([text], { type: "text/plain" });
+    // creating a url
     const url = URL.createObjectURL(blob);
+    // creating a link
     const link = document.createElement("a");
+    // setting the attributes of the link
     link.setAttribute("href", url);
+    // setting the attributes of the link
     link.setAttribute("download", filename);
+    // clicking the link
     link.click();
-    // generate the date and time
 }
 
 
@@ -66,10 +75,10 @@ checkboxes.forEach((checkbox) => {
         if (!checked) {
             document.querySelector('#abc').checked = true;
         }
-        // add the checked to a list
     });
 });
 
+// adding the change event to the slider
 slider.addEventListener("input", () => {
     if (slider.value < 1) {
         alert("slider value cannot be less than 1")
@@ -79,8 +88,7 @@ slider.addEventListener("input", () => {
     }
 })
 
-let data = abc.concat(ABC, numbers, symbols);
-
+// adding the click event to the generate button
 generate.addEventListener("click", () => {
     // get the checked checkboxes
     let checkedData = Array.from(checkboxes).filter((checkbox) => checkbox.checked);
@@ -97,6 +105,7 @@ generate.addEventListener("click", () => {
     text2.textContent = passwordGenerator(randomArray, sliderValue);
 })
 
+// functions to copy text to clipboard
 icon1.addEventListener("click", () => {
     // copy the text to clipboard
     navigator.clipboard.writeText(text1.textContent);
@@ -109,6 +118,7 @@ icon2.addEventListener("click", () => {
     alert("Copied to clipboard")
 })
 
+// functions to take the inputs from the user and store it in text files which the user downloads
 download1.addEventListener("click", () => {
     let fileName = prompt("Please enter name of the file", "password");
     let reason = prompt("Please enter reason for generating password", "for testing");
